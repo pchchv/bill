39124c80b30fc78c8888268b9ec1704fdc8824b7
pchchv <ipchchv@gmail.com>
2022-09-22 13:49:56 +0300

    Implement data retrieval from an environment file

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Bill struct {
 	list          []Item  // List of ordered items
 	totalQuantity int     // Total number of items ordered
@@ -16,6 +23,22 @@ type Item struct {
 type KitchenList struct {
 	list          []Item
 	totalQuantity int
+}
+
+func init() {
+	// Load values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value. Outputs a panic if the value is missing
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("Value %v does not exist", v)
+	}
+	return value
 }
 
 func main() {}
